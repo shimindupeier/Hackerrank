19e7494723ebbbcd11528931d3371a17824c99dd
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-07 23:56:40 +0100

    compute sum using flapmap and function

--- src/main/kotlin/2DArrays.kt
+++ src/main/kotlin/2DArrays.kt
@@ -5,11 +5,16 @@ fun main(args: Array<String>) {
     for (i in 0 until 6) {
         arr[i] = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
     }
-    
-    val temp = (0 until 4).map { row -> (0 until 4).map { col -> val sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2] +
-            arr[row + 1][col + 1] +
-            arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2]
+
+    val temp = (0 until 4).flatMap { row -> (0 until 4).map { col -> val sum = computeSum(arr, row, col)
         sum
-    } }.flatten()
+    } }
     println(temp.maxOrNull())
 }
+
+private fun computeSum(arr: Array<Array<Int>>, row: Int, col: Int) =
+    arr[row][col] + arr[row][col + 1] + arr[row][col + 2] +
+            arr[row + 1][col + 1] +
+            arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2]
+
+
