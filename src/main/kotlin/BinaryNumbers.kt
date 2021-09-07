5fb77283a640a1ab80f4301bd319f19267033850
shimindupeier <87096821+shimindupeier@users.noreply.github.com>
2021-09-08 00:36:06 +0100

    refactored

--- src/main/kotlin/BinaryNumbers.kt
+++ src/main/kotlin/BinaryNumbers.kt
@@ -2,17 +2,16 @@ fun main() {
     val n = readLine()!!.toInt()
     val binaryList = mutableListOf<Int>()
     var counter = 0
-    var maximum = 0
     var dividend = n
     while (dividend > 0) {
         val remainder = dividend.rem(2)
         dividend /= 2
         if (remainder == 1) counter++
         else {
-            if (counter > maximum) maximum = counter
+            binaryList.add(counter)
             counter = 0
         }
-        if (dividend == 0 && counter > maximum) maximum = counter
+        if (dividend == 0) binaryList.add(counter)
     }
-    println(maximum)
+    println(binaryList.maxOrNull())
 }
