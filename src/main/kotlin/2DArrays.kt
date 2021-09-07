58ad9dec0f49a783a8728a2c8527a9c0b5162dc2
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-07 23:55:46 +0100

    compute sum using map

--- src/main/kotlin/2DArrays.kt
+++ src/main/kotlin/2DArrays.kt
@@ -1,29 +1,15 @@
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
-
-
-
 fun main(args: Array<String>) {
 
     val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
 
     for (i in 0 until 6) {
-        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+        arr[i] = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
     }
+    
+    val temp = (0 until 4).map { row -> (0 until 4).map { col -> val sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2] +
+            arr[row + 1][col + 1] +
+            arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2]
+        sum
+    } }.flatten()
+    println(temp.maxOrNull())
 }
